7685b3a04424462ff23fe5a7ceaf0fd0b2d30013
Deivis-M <davidminango2009@gmail.com>
2026-08-23 11:15:17 -0500

    Rectangulo and TestRectangulo encapsulado e invocadp

---
+++ Rectangulo.java
@@ -0,0 +1,37 @@
+package com.krakedev;
+
+public class Rectangulo {
+
+	private int base ;
+	private int altura ;
+	
+	public int calcularArea() {
+		
+		int area=base*altura;
+		
+		return area ;
+	}
+	
+	public double calcularPerimetro() {
+		double perimetro = (base+altura)*2;
+		return perimetro;
+	}
+
+	public int getBase() {
+		return base;
+	}
+
+	public void setBase(int base) {
+		this.base = base;
+	}
+
+	public int getAltura() {
+		return altura;
+	}
+
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+	
+	
+}
